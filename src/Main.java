ddf024d308db8bce15eab5957ed954a82174893d
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-03-25 20:46:13 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int wholeNumbers = 122;
+        long largeDecimalNumber = -12;
+        System.out.println();
+    }
+}
