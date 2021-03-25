d5f1e5d7ddbe4e5109d6f44c869db13f1b10e3fb
Melvin Baybayanon <melvsbaybayanon@gmail.com>
2021-03-25 20:55:07 +0800

    Added Code

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,22 @@ public class Main {
     public static void main(String[] args) {
         int wholeNumbers = 122;
         long largeDecimalNumber = -12;
-        System.out.println();
+        boolean isEqualTo = true;
+        char singleLetter = 'a';
+        byte smallNumbers  = 12;
+        float intergralNumber = 12.02f;
+        double largeIntergralNumber = 12.0423d;
+
+//        Primitive Data Types
+        System.out.println(wholeNumbers);
+        System.out.println(largeDecimalNumber);
+        System.out.println(isEqualTo);
+        System.out.println(singleLetter);
+        System.out.println(smallNumbers);
+        System.out.println(intergralNumber);
+        System.out.println(largeIntergralNumber);
+
+//        Non Primitive Data Type
+
     }
 }
